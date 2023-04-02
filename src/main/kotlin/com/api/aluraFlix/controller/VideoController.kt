629399775c3b131ed88ccb67f2a5f4c01114e956
Gabriela Assis <gabriela_assis96@hotmail.com>
2023-04-01 22:18:41 -0300

    Inclui requisição GET que chama pelo id do video

--- src/main/kotlin/com/api/aluraFlix/controller/VideoController.kt
+++ src/main/kotlin/com/api/aluraFlix/controller/VideoController.kt
@@ -3,6 +3,7 @@ package com.api.aluraFlix.controller
 import com.api.aluraFlix.model.Video
 import com.api.aluraFlix.service.VideoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,5 +16,10 @@ class VideoController(
     @GetMapping
     fun getVideos(): List<Video> {
         return videoService.listar()
+    }
+
+    @GetMapping("/{id}")
+    fun buscarVideoPorId(@PathVariable id: Long): Video{
+        return videoService.buscarPorId(id)
     }
 }
--- src/main/kotlin/com/api/aluraFlix/service/VideoService.kt
+++ src/main/kotlin/com/api/aluraFlix/service/VideoService.kt
@@ -4,10 +4,43 @@ import com.api.aluraFlix.model.Video
 import org.springframework.stereotype.Service
 
 @Service
-class VideoService {
-    fun listar(): List<Video> {
-        val video = (Video(id = null, titulo = "Morango", descricao = "Criando morangos", url = "/"))
+class VideoService(private var listaVideos: List<Video>) {
 
-        return listOf(video, video, video)
+    init {
+        val video1 = (Video
+            (
+            id = 1,
+            titulo = "COISAS DA MODA",
+            descricao = "Tendencias da moda 2000 ",
+            url = "/"
+        ))
+
+        val video2 = (Video
+            (
+            id = 2,
+            titulo = "RECEITA DE BOLO",
+            descricao = "Receita da vovó Cicera",
+            url = "/"
+        ))
+
+        val video3 = (Video
+            (
+            id = 3,
+            titulo = "OS MELHORES FILMES DE COMÉDIA",
+            descricao = "Listando os melhores filmes do ano!",
+            url = "/"
+        ))
+
+        listaVideos = listOf(video1, video2, video3)
+    }
+
+    fun listar(): List<Video> {
+        return listaVideos
+    }
+
+    fun buscarPorId(id: Long): Video {
+        return listaVideos.stream().filter { video ->
+            video.id == id
+        }.findFirst().get()
     }
 }
